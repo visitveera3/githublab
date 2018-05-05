25efbaeba7d3b690bad0d84f8b17254c65cf49bd
GitUser-1 <GitUser-1@DevOps.com>
2018-05-05 13:14:18 -0400

    Changed the name of the function as pointer type threw error

--- string.c
+++ string.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-size_t my_strlen(char *s)
+size_t my_strlength(char *s)
 {
-char *p = s;
+ char *p = s;
 while (*p)
 ++p;
 return (p - s);
@@ -18,6 +18,6 @@ char *s[] =
 };
 
 for (i = 0; i < 2; ++i)
-printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+printf("string lenght of %s = %d\n", s[i], my_strlength(s[i]));
 return 0;
 }
